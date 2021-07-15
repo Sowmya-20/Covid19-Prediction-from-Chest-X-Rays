d7c841c679ba4b6b27ad3aef0c3b7f17a21cc275
Sowmya Jonnalagadda <jonnalagaddasowmya123@gmail.com>
2021-07-15 21:11:17 +0530

    Covid19_Prediction

---
+++ app.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+from tensorflow.keras.models import load_model
+#from keras.models import load_model
+import numpy as np
+from flask import Flask, request, redirect, url_for, send_from_directory,render_template
+from keras.preprocessing import image
+from flask_ngrok import run_with_ngrok
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+
+import cv2
+
+
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+model_path="model/covid_pred_model.h5"
+model=load_model(model_path,compile=False)
+
+img_size=100
+
+
+def model_predict(img_path, model):
+
+    test_image=image.load_img(img_path,color_mode="grayscale",target_size=(100,100))
+    test_image=image.img_to_array(test_image)
+    test_image=np.expand_dims(test_image,axis=0)
+    res=model.predict(test_image)[0][0]
+    return res
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/upload', methods=["GET","POST"])
+def upload():
+    if request.method == 'POST':
+        file = request.files['image_file']
+        basepath=os.path.dirname(__file__)
+        filename=secure_filename(file.filename)
+        filepath=os.path.join(basepath,'uploads/',file.filename)
+        file.save(filepath)
+        print(filepath)
+        livepreds = model_predict(filepath,model)
+        if livepreds==1:
+            return render_template('covid negative.html',filename=filename)
+        else:
+            return render_template('Covid positive.html',filename=filename)
+    return None
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    #print('display_image filename: ' + filename)
+    return send_from_directory("uploads", filename)
+if __name__ == '__main__':
+    app.run()
+          
+        
+
